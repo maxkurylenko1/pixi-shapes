63869a1b95c769f27d094977890c34d4bed81914
Maksym Kurylenko <maxkurylenko1@gmail.com>
2026-03-27 18:38:50 +0100

    feat: add boot scene with progress bar

--- src/app/App.ts
+++ src/app/App.ts
@@ -5,6 +5,7 @@ import { GameController } from '../controller/GameController';
 import { InputController } from '../controller/InputController';
 import { ControlsController } from '../controller/ControlsController';
 import { HudView } from '../view/HudView';
+import { BootScene } from '../view/BootScene';
 import { AREA_WIDTH, AREA_HEIGHT } from '../utils/constants';
 
 export class App {
@@ -17,26 +18,38 @@ export class App {
   protected controlsController!: ControlsController;
 
   async init(): Promise<void> {
-    this.pixiApp = new Application();
+    const boot = new BootScene();
+    boot.setProgress(5);
 
+    this.pixiApp = new Application();
     await this.pixiApp.init({
       width: AREA_WIDTH,
       height: AREA_HEIGHT,
       backgroundColor: 0x0d0d1a,
       antialias: true,
     });
+    boot.setProgress(45);
 
     const container = document.getElementById('canvas-container');
     if (!container) throw new Error('#canvas-container not found');
     container.appendChild(this.pixiApp.canvas);
+    boot.setProgress(60);
 
     this.model = new GameModel();
     this.view = new GameView(this.pixiApp);
     this.hudView = new HudView();
+    boot.setProgress(78);
+
     this.controller = new GameController(this.model, this.view, this.hudView);
     this.inputController = new InputController(this.model, this.view);
     this.controlsController = new ControlsController(this.model);
+    boot.setProgress(92);
 
     this.controller.start();
+
+    await boot.hide();
+
+    const app = document.getElementById('app');
+    if (app) app.style.opacity = '1';
   }
 }
